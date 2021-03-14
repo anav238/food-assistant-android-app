a4bf468d912ad027512e779a160f8dbf378d39a9
anav238 <anavasiliu139@gmail.com>
2021-03-14 21:59:07 +0200

    added toolbar

--- app/src/main/java/com/example/food_assistant/DayNutrition.kt
+++ app/src/main/java/com/example/food_assistant/DayNutrition.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [dayNutrition.newInstance] factory method to
+ * Use the [DayNutrition.newInstance] factory method to
  * create an instance of this fragment.
  */
-class dayNutrition : Fragment() {
+class DayNutrition : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -47,7 +47,7 @@ class dayNutrition : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                dayNutrition().apply {
+                DayNutrition().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/food_assistant/MainActivity.kt
+++ app/src/main/java/com/example/food_assistant/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.food_assistant
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.app_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
