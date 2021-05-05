4abaf9a5f2d1f059b5928285327f6c091bad8d95
anav238 <anavasiliu139@gmail.com>
2021-05-05 19:38:48 +0300

    test

--- app/src/main/java/com/example/food_assistant/Activities/MainActivity.java
+++ app/src/main/java/com/example/food_assistant/Activities/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.i("Test", user.toString());
                 //UserDataLogger.logUserData(user);
             }
+            //test
         }
     }
 
